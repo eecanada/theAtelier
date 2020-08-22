3b8b8f66a10ae6636e14d40107ac543e0ab81303
Eder Canada <canada.eder@gmail.com>
2020-08-21 22:50:14 -0700

    init

---
+++ gatsby-starter-eventually/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Eventually', // <title>
+  manifestName: 'Eventually',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'The Atelier',
+  subHeading:
+    "To design and build elegant websites.",
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/eecanada?tab=overview&from=2020-03-01&to=2020-03-06',
+    },
+    {
+      icon: 'fa-yelp',
+      name: 'Twitter',
+      url: 'https://yelp.to/qTKq/z5NE6hYB08',
+    },
+    {
+      icon: 'fa-instagram',
+      name: 'Facebook',
+      url: 'https://www.instagram.com/theatelier.io/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:canada.eder@gmail.com',
+    },
+  ],
+};
