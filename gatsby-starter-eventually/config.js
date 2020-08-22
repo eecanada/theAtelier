2f892dfeabcb29de6323988aaee5b62df3ffd010
Eder Canada <canada.eder@gmail.com>
2020-08-21 23:02:30 -0700

    site title

--- gatsby-starter-eventually/config.js
+++ gatsby-starter-eventually/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Eventually', // <title>
+  siteTitle: 'The Atelier - Web Developement Agency', // <title>
   manifestName: 'Eventually',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
